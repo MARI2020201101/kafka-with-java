e9c84b17860a09d1fc3bfd17a99077847c9d57df
MARI <mari20201101@gmail.com>
2022-02-19 02:54:19 +0900

    Create first kafka producer with java application

---
+++ src/main/java/com/mariworld/kafka/ProducerDemoV2.java
@@ -0,0 +1,31 @@
+package com.mariworld.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemoV2 {
+
+    private static final Logger logger = LoggerFactory.getLogger(ProducerDemoV2.class);
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG ,StringSerializer.class.getName());
+
+        KafkaProducer<String,String> producer = new KafkaProducer<String, String>(properties);
+
+        ProducerRecord<String,String> record = new ProducerRecord<>("first_topic","hello world");
+
+        producer.send(record);
+        logger.info("{}", record);
+
+        producer.flush();
+        producer.close();
+    }
+}
