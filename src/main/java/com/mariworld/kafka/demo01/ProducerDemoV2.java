01ef3f62c1932bbb80ce0fc2e6f17184df67597f
MARI <mari20201101@gmail.com>
2022-02-19 10:12:30 +0900

    Organize project with module architecture

---
+++ src/main/java/com/mariworld/kafka/demo01/ProducerDemoV2.java
@@ -0,0 +1,44 @@
+package com.mariworld.kafka.demo01;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemoV2 {
+
+    private static final Logger logger = LoggerFactory.getLogger(ProducerDemoV2.class);
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG ,StringSerializer.class.getName());
+
+        KafkaProducer<String,String> producer = new KafkaProducer<String, String>(properties);
+
+        for (int i = 0; i < 10; i++) {
+            ProducerRecord<String,String> record = new ProducerRecord<>("first_topic","hello world [ " + i + " ]");
+
+            producer.send(record, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    logger.info("recordMetadata --> {}", recordMetadata);
+                    logger.info(" {} \n {} \n {} \n {}",
+                            recordMetadata.topic(),
+                            recordMetadata.partition(),
+                            recordMetadata.offset(),
+                            recordMetadata.timestamp());
+                }
+            });
+            logger.warn("record --> {}", record);
+            logger.error("record --> {}", record);
+        }
+
+
+
+        producer.flush();
+        producer.close();
+    }
+}
