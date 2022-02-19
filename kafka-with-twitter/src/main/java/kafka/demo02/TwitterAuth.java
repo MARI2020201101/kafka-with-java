cd44a414ea945a1f4eb7fd3a357f4de571e6710e
MARI <mari20201101@gmail.com>
2022-02-19 10:06:32 +0900

    Modify gitignore

---
+++ kafka-with-twitter/src/main/java/kafka/demo02/TwitterAuth.java
@@ -0,0 +1,18 @@
+package com.mariworld.kafka.demo02;
+
+public enum TwitterAuth {
+    AUTH_KEY("xU4tKcF2rbv10eG7q3q3NPsKT")
+    , AUTH_SECRET("2CrF4iVIAFgfT7Dnm4jx0jWBX5Y2qijCnopfRZrEOUrY9JiNkb")
+    , AUTH_TOKEN("1494768720228478978-yaGT9xAdxlJEGpcbvjVbeWfP5k8H5z")
+    , AUTH_TOKEN_SECRET("yvfTriSUTdAkAD7TEgDxhKZdiuv3FOUPxWERDh19KIavG");
+
+    private String value;
+
+    TwitterAuth(String value){
+        this.value=value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
